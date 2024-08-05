5addb9fbb365272e4c31bb4a2913b179679a57a2
mimmbo <dol.vlad97@gmail.com>
2024-08-05 20:16:56 +0200

    init

---
+++ js/2024 plan/lesson1_vars_types_cats/TASKS/init.js
@@ -0,0 +1,42 @@
+let a = 'Hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let boll1 = true;
+let boll2 = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(boll1);
+console.log(boll2);
+let firstname = 'Vladyslav';
+let middlename = 'Rostyslavovych';
+let lastname ='Dolynchuk';
+let person = firstname + ' ' + middlename + ' ' + lastname;
+console.log(person)
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1)
+console.log(typeof b1)
+console.log(typeof c1)
+let firstname1 = prompt("Введіть Ім'я:");
+let middlename1 = prompt("Введіть По-Батькові:");
+let age = prompt("Введіть вік:");
+console.log(firstname1 + ' ' + middlename1 + ' ' + age);
+
